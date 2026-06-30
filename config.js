97bef71d0fae0cd3744fd15d4ff71fe6b2fa3db9
coolachin94-blip <coolachin94@gmail.com>
2026-06-30 22:53:24 +0900

    8.2

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 // TAJIMA 生産管理 設定ファイル
 // SupabaseのPublishable keyを下の "" の中に入れてください。
-// 次回以降の更新では、この config.js は基本的に上書きしないでください。
-
 window.TAJIMA_CONFIG = {
   SUPABASE_URL: "https://kufexonxsbscixaauwue.supabase.co",
   SUPABASE_KEY: "sb_publishable_s3E7Lq5UtP_sfCnPwuHZBA_EfKeQVdh",
