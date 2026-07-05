1eafbaecc637dd17027b41fe5b667e0a920de221
coolachin94-blip <coolachin94@gmail.com>
2026-07-05 22:20:01 +0900

    8.5

--- config.js
+++ config.js
@@ -1,7 +1,1 @@
-// TAJIMA 生産管理 設定ファイル
-// SupabaseのPublishable keyを下の "" の中に入れてください。
-window.TAJIMA_CONFIG = {
-  SUPABASE_URL: "https://kufexonxsbscixaauwue.supabase.co",
-  SUPABASE_KEY: "sb_publishable_s3E7Lq5UtP_sfCnPwuHZBA_EfKeQVdh",
-  DEFAULT_ADMIN_PASSWORD: "0000"
-};
+window.TAJIMA_CONFIG={SUPABASE_URL:"https://kufexonxsbscixaauwue.supabase.co",SUPABASE_KEY:"sb_publishable_s3E7Lq5UtP_sfCnPwuHZBA_EfKeQVdh",DEFAULT_ADMIN_PASSWORD:"0000"};
